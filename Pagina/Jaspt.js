7151118b4e47b1794803f8ce968bb5fe9c0d0be8
nvasquez97 <n.vasquez10@uniandes.edu.co>
2017-02-14 22:30:04 -0500

    Pagina varios terminada, falta aboutme

--- Pagina/Jaspt.js
+++ Pagina/Jaspt.js
@@ -45,59 +45,138 @@ function onClickRandomBtn()
 }
 
   // Tomado de https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function randomBetween(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+  function randomBetween(min, max) {
+  	min = Math.ceil(min);
+  	max = Math.floor(max);
+  	return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-function onClickRandomAll()
-{
-	
-	var d =randomBetween(0,3);
-	var b = document.getElementsByClassName("randomT")[0];
-	if(d!==0)
-	{
-		b.src="imgs/prin"+d+".jpg"
+  function onClickRandomAll()
+  {
 
-	}
-	else{
-	b.src="imgs/prin.jpg"
-	}	
+  	var d =randomBetween(0,3);
+  	var b = document.getElementsByClassName("randomT")[0];
+  	if(d!==0)
+  	{
+  		b.src="imgs/prin"+d+".jpg";
 
-	var c =randomBetween(0,6);
-	var z =document.getElementsByClassName("randomimg")[0];
-	if(c!==0)
-	{
-		z.src="imgs/prof"+c+".jpeg"	
-	}
-	else
-	{
-		z.src="imgs/prof.jpeg"		
-	}
+  	}
+  	else{
+  		b.src="imgs/prin.jpg";
+  	}	
 
-	if(c===1)
-	{
-		
-	}
-	else if (c===2)
-	{ 
-		
-	}
-	else if(c===3)
-	{
-		
-	}
-	else if(c===4)
-	{
-		
-	}
-	else if(c===5)
-	{
-		
-	}
-	else{
-		
-	}
-	
-}
+  	var c =randomBetween(0,6);
+  	var z =document.getElementsByClassName("randomimg")[0];
+
+  	if(c!==0)
+  	{
+  		z.src="imgs/prof"+c+".jpeg"	;
+  	}
+  	else
+  	{
+  		z.src="imgs/prof.jpeg";
+  	}
+
+  	if(c===1)
+  	{
+  		let border = document.getElementsByClassName("random");
+  		let head = document.getElementsByClassName("randomh");
+  		let text = document.getElementsByClassName("randomtxt")[0];
+
+  		let numberR = randomBetween(0,4);
+  		for(i=0;i<border.length;i++)
+  		{
+  			border[i].style.borderColor="black";
+  			head[i].style.color="purple";
+  			head[i].innerHTML="?";
+  		}
+  		head[0].innerHTML ="Awesome!";
+
+  		text.style.color="black";
+  		text.style.fontFamily="cursive";
+
+  		head[numberR+1].innerHTML="<i class=\"fa fa-cog\" aria-hidden=\"true\">"+" Don't ask why";
+  	}
+  	else if (c===2)
+  	{ 
+  		let border = document.getElementsByClassName("random");
+  		let head = document.getElementsByClassName("randomh");
+  		let text = document.getElementsByClassName("randomtxt")[0];
+  		let numberR = randomBetween(0,4);
+  		for(i=0;i<border.length;i++)
+  		{
+  			border[i].style.borderColor="white";
+  			head[i].style.color="brown";
+  			head[i].innerHTML="?";
+  		}
+  		head[0].innerHTML ="Great!";
+
+  		head[numberR+1].innerHTML="<i class=\"fa fa-cog\" aria-hidden=\"true\">"+" But it works!";
+  		text.style.color="white";
+  		text.style.fontWeight="bold";
+  	}
+  	else if(c===3)
+  	{
+		let border = document.getElementsByClassName("random");
+		let head = document.getElementsByClassName("randomh");
+		let text = document.getElementsByClassName("randomtxt")[0];
+		let numberR = randomBetween(0,4);
+  		for(i=0;i<border.length;i++)
+  		{
+  			border[i].style.borderColor="red";
+  			head[i].style.color="black";
+  			head[i].innerHTML="?";
+  		}
+		head[0].innerHTML ="Enjoy!";
+		head[numberR+1].innerHTML="<i class=\"fa fa-gear\" aria-hidden=\"true\">"+" It just works";
+		text.style.fontWeight="lighter";
+  	}
+  	else if(c===4)
+  	{
+  		let border = document.getElementsByClassName("random");
+  		let head = document.getElementsByClassName("randomh");
+  		let text = document.getElementsByClassName("randomtxt")[0];
+  		let numberR = randomBetween(0,4);
+  		for(i=0;i<border.length;i++)
+  		{
+  			border[i].style.borderColor="lightcoral";
+  			head[i].style.color="blue";
+  			head[i].innerHTML="?";
+  		}
+  		head[0].innerHTML ="Javascript works!";
+  		head[numberR+1].innerHTML="<i class=\"fa fa-flag\" aria-hidden=\"true\">"+" This is my random page";
+  		text.style.fontFamily="fantasy";
+  	}
+  	else if(c===5)
+  	{
+  		let border = document.getElementsByClassName("random");
+  		let head = document.getElementsByClassName("randomh");
+  		let text = document.getElementsByClassName("randomtxt")[0];
+  		let numberR = randomBetween(0,4);
+  		for(i=0;i<border.length;i++)
+  		{
+  			border[i].style.borderColor="blue";
+  			head[i].style.color="lightcoral";
+  			head[i].innerHTML="?";
+  		}
+  		head[0].innerHTML ="WebDev!";
+  		head[numberR+1].innerHTML="<i class=\"fa fa-heartbeat\" aria-hidden=\"true\">"+" Love JS";
+  		text.style.color="#800200";
+  	}
+  	else{
+  		let border = document.getElementsByClassName("random");
+  		let head = document.getElementsByClassName("randomh");
+  		let text = document.getElementsByClassName("randomtxt")[0];
+  		let numberR = randomBetween(0,4);
+  		for(i=0;i<border.length;i++)
+  		{
+  			border[i].style.borderColor="green";
+  			head[i].style.color="black";
+  			head[i].innerHTML="?";
+  		}
+  		head[0].innerHTML ="Thanks for clicking!";
+  		head[numberR+1].innerHTML="<i class=\"fa fa-gears\" aria-hidden=\"true\">"+" Javasrcipt is weird";
+  		text.style.fontFamily="monospace";
+  	}
+
+  }
